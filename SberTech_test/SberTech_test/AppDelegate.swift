b803db72218d25b403c81bd7b23c8b296e97cbb9
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-09-05 21:32:40 +0300

    Started project

---
+++ SberTech_test/SberTech_test/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  SberTech_test
+//
+//  Created by Evgeniy Akhmerov on 05/09/2017.
+//  Copyright © 2017 Evgeniy Akhmerov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    // MARK: - UIApplicationDelegate
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
