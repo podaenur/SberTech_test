230352c86f386d25d7bacc82315c84cb66c5a1d3
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-09-06 22:38:30 +0300

    Implemented font and color helpers

---
+++ SberTech_test/SberTech_test/UIColor+Helper.swift
@@ -0,0 +1,32 @@
+//
+//  UIColor+Helper.swift
+//  SberTech_test
+//
+//  Created by Evgeniy Akhmerov on 06/09/2017.
+//  Copyright © 2017 Evgeniy Akhmerov. All rights reserved.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+    
+    static var sb_white: UIColor {
+        return .white
+    }
+    
+    static var sb_title: UIColor {
+        return .black
+    }
+    
+    static var sb_detail: UIColor {
+        return .lightGray
+    }
+    
+    static var sb_border: UIColor {
+        return .darkGray
+    }
+    
+    static var sb_backgroundTable: UIColor {
+        return UIColor(red:0.85, green:0.85, blue:0.85, alpha:1.00)
+    }
+}
---
+++ SberTech_test/SberTech_test/UIFont+Helper.swift
@@ -0,0 +1,20 @@
+//
+//  UIFont+Helper.swift
+//  SberTech_test
+//
+//  Created by Evgeniy Akhmerov on 06/09/2017.
+//  Copyright © 2017 Evgeniy Akhmerov. All rights reserved.
+//
+
+import UIKit.UIFont
+
+extension UIFont {
+    
+    static var sb_title: UIFont {
+        return .boldSystemFont(ofSize: 18)
+    }
+    
+    static var sb_detail: UIFont {
+        return .systemFont(ofSize: 18)
+    }
+}
