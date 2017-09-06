74a1aec16f474c7529274aeb0029ad3c56077b75
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-09-06 19:23:57 +0300

    Implemented network client

---
+++ SberTech_test/SberTech_test/NetworkClient.swift
@@ -0,0 +1,60 @@
+//
+//  NetworkClient.swift
+//  SberTech_test
+//
+//  Created by Evgeniy Akhmerov on 05/09/2017.
+//  Copyright © 2017 Evgeniy Akhmerov. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkResponse {
+    case success(Data?)
+    case failure(Error)
+}
+
+enum FetchType: String {
+    case organisations = "getOrganizationListTest_1"
+    case visits = "getVisitsListTest_1"
+}
+
+typealias FetchCompletion = (NetworkResponse) -> Void
+
+class NetworkClient {
+    
+    // MARK: - Properties
+    
+    private let networkQueue = DispatchQueue(label: "com.e-legion.NetworkClient.queue")
+    private let baseURL: URL
+    private let session: URLSession
+    
+    // MARK: - Life cycle
+    
+    init(baseURL: URL) {
+        self.baseURL = baseURL
+        self.session = URLSession(configuration: .default)
+    }
+    
+    // MARK: - Public
+    
+    func fetch(_ type: FetchType, completion: @escaping FetchCompletion) {
+        let request = URLRequest(url: baseURL.appendingPathComponent(type.rawValue))
+        execute(request: request, completion: completion)
+    }
+    
+    // MARK: - Private
+    
+    private func execute(request: URLRequest, completion: @escaping FetchCompletion) {
+        networkQueue.async {
+            let task = self.session.dataTask(with: request, completionHandler: {
+                (data, _, error) in
+                if let _error = error {
+                    completion(.failure(_error))
+                } else {
+                    completion(.success(data))
+                }
+            })
+            task.resume()
+        }
+    }
+}
