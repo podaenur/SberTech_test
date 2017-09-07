fee1dd4ba79aded465ac28e6f89ba2bc57de71f6
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-09-07 23:56:36 +0300

    Adapted main view controller instantiating to use screen factory

--- SberTech_test/SberTech_test/AppDelegate.swift
+++ SberTech_test/SberTech_test/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window!.rootViewController = MainController()
+        window!.rootViewController = ScreensFactory.mainScreen(context: nil)
         window!.makeKeyAndVisible()
         
         return true
