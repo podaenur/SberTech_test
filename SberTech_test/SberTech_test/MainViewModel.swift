91558cebce18a2c695a6aacb51e8b4770fff7c3b
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-09-07 23:54:50 +0300

    Implemented main view model

---
+++ SberTech_test/SberTech_test/MainViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewModel.swift
+//  SberTech_test
+//
+//  Created by Evgeniy Akhmerov on 07/09/2017.
+//  Copyright © 2017 Evgeniy Akhmerov. All rights reserved.
+//
+
+import Foundation
+
+class MainViewModel: BaseViewModel {
+    //
+}
