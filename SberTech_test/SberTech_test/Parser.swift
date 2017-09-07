15dbf3741b331439c50bf3eb44cde86a85f1086a
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-09-07 14:43:44 +0300

    Implemented JSON parser

---
+++ SberTech_test/SberTech_test/Parser.swift
@@ -0,0 +1,30 @@
+//
+//  Parser.swift
+//  SberTech_test
+//
+//  Created by Evgeniy Akhmerov on 07/09/2017.
+//  Copyright © 2017 Evgeniy Akhmerov. All rights reserved.
+//
+
+import Foundation
+
+struct Parser {
+    
+    // MARK: - Properties
+    
+    private var dataTypeError: Error = {
+        let userInfo: [AnyHashable: Any?] = [NSLocalizedDescriptionKey: "Parser reached wrong data type in JSON." as Any]
+        return NSError(codedDomain: NSError.Domains.parser, userInfo: userInfo) as Error
+    }()
+    
+    // MARK: - Public
+    
+    func parse<T: JSONInitializable>(_ data: Data) throws -> [T] {
+        let json = try JSONSerialization.jsonObject(with: data) as! Array<[String: Any]>
+        let mapped: [T] = try json.map({
+            guard let model = T(json: $0) else { throw dataTypeError }
+            return model
+        })
+        return mapped
+    }
+}
