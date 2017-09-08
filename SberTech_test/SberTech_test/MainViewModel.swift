f068546da11e4983493f0edeaf7b1a239ea950ab
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-09-08 10:21:53 +0300

    Implemented data logic in main view model

--- SberTech_test/SberTech_test/MainViewModel.swift
+++ SberTech_test/SberTech_test/MainViewModel.swift
@@ -9,5 +9,75 @@
 import Foundation
 
 class MainViewModel: BaseViewModel {
-    //
+    
+    // MARK: - Properties
+    
+    private let dataManager = DataManager.shared
+    private var organizationModels = OrganizationModelPairs()
+    private var visitModels = [VisitModel]()
+    
+    var numberOfRows: Int {
+        return visitModels.count
+    }
+    var dataDidLoad: (() -> Void)?
+    
+    // MARK: - Life cycle
+    
+    override func initialSetup() {
+        super.initialSetup()
+        
+        loadAllData()
+    }
+    
+    // MARK: - Public
+    
+    func cellModel(at indexPath: IndexPath) -> MainViewCellModel {
+        let visit = visitModels[indexPath.row]
+        let organization = organizationModels[visit.identifier]
+        return MainViewCellModel(title: visit.title, detail: organization?.title)
+    }
+    
+    // MARK: - Private
+    
+    private func loadAllData() {
+        dataManager.getOrganizations {
+            [weak self] (response) in
+            guard let sSelf = self else { return }
+            
+            switch response {
+            case .success(let models):
+                DispatchQueue.main.async {
+                    sSelf.organizationModels = models
+                }
+                sSelf.loadVisits()
+                
+            case .failure(let error):
+                sSelf.handle(error)
+            }
+        }
+    }
+    
+    private func loadVisits() {
+        dataManager.getVisits {
+            [weak self] (response) in
+            guard let sSelf = self else { return }
+            
+            switch response {
+            case .success(let models):
+                DispatchQueue.main.async {
+                    sSelf.visitModels = models
+                    sSelf.dataDidLoad?()
+                }
+                
+            case .failure(let error):
+                sSelf.handle(error)
+            }
+        }
+    }
+    
+    private func handle(_ error: Error) {
+        DispatchQueue.main.async {
+            self.showError?(error)
+        }
+    }
 }
